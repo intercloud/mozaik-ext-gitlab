89bf4594706ca9059b8d2aa289f3592bd0bfdb27
Antonio <georges-antonio.indrianjafy@intercloud.com>
2018-12-27 17:54:03 +0100

    fix

--- src/components/PipelinesHistory.jsx
+++ src/components/PipelinesHistory.jsx
@@ -1,61 +1,56 @@
-import React, { Component, PropTypes } from 'react';
-import reactMixin                      from 'react-mixin';
-import { ListenerMixin }               from 'reflux';
-import Mozaik                          from 'mozaik/browser';
-
+import React, { Component, PropTypes } from "react";
+import reactMixin from "react-mixin";
+import { ListenerMixin } from "reflux";
+import Mozaik from "mozaik/browser";
 
 class PipelinesHistory extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            project: null,
-            pipelines:  []
-        };
-    }
+    this.state = {
+      project: null,
+      pipelines: []
+    };
+  }
 
-    getApiRequest() {
-        const { project } = this.props;
+  getApiRequest() {
+    const { project } = this.props;
 
-        return {
-            id:     `gitlab.projectPipelines.${project}`,
-            params: { project }
-        };
-    }
+    return {
+      id: `gitlab.projectPipelines.${project}`,
+      params: { project }
+    };
+  }
 
-    onApiData({ project, pipelines }) {
-        this.setState({ project, pipelines });
-    }
+  onApiData(data) {
+    const { project, pipelines } = data;
+    console.log(data);
+    this.setState({ project, pipelines });
+  }
 
-    render() {
-        const { project, pipelines } = this.state;
+  render() {
+    const { project, pipelines } = this.state;
+    console.log(pipelines);
 
-        return (
-            <div>
-                <div className="widget__header">
-                    pipelines history
-                    <i className="fa fa-bars" />
-                </div>
-                <div className="widget__body">
-                    {JSON.stringify(pipelines)}
-                    {pipelines[0].status}
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <div className="widget__header">
+          pipelines history
+          <i className="fa fa-bars" />
+        </div>
+        <div className="widget__body">{JSON.stringify(pipelines)}</div>
+      </div>
+    );
+  }
 }
 
-PipelinesHistory.displayName = 'PipelinesHistory';
+PipelinesHistory.displayName = "PipelinesHistory";
 
 PipelinesHistory.propTypes = {
-    project: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ]).isRequired
+  project: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
 };
 
 reactMixin(PipelinesHistory.prototype, ListenerMixin);
 reactMixin(PipelinesHistory.prototype, Mozaik.Mixin.ApiConsumer);
-
 
 export default PipelinesHistory;
