7077ddf5724e131bb212110e29bf0cfe4c489281
Antonio <georges-antonio.indrianjafy@intercloud.com>
2018-12-28 10:58:18 +0100

    fix

--- src/components/PipelinesHistory.jsx
+++ src/components/PipelinesHistory.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import reactMixin from 'react-mixin';
 import { ListenerMixin } from 'reflux';
 import Mozaik from 'mozaik/browser';
-import PipelineHistoryItem from './BuildHistoryItem.jsx';
+import PipelineHistoryItem from './PipelineHistoryItem.jsx';
 
 class PipelinesHistory extends Component {
 	constructor(props) {
